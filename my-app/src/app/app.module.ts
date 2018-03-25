7ae94ce4ebd5ad4f2d305e68a32ce277106a3104
gao <jackiegao2018@hotmail.com>
2018-03-24 23:12:54 -0400

    add recipe module and share module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from "./shared/dropdown.directive";
 import { RecipeService } from './recipes/recipes.service';
 import { ShoppingListService } from './shopping-list/shoppinglist.service';
 
-import { HttpModule } from '@angular/http';
 import {DataStorageService} from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -25,29 +19,24 @@ import { AuthService } from './auth/auth.service';
 import {AuthGuard} from "./auth/auth-guard.service";
 import Auth = firebase.auth.Auth;
 
+import { RecipeModule } from './recipes/recipe.module';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeEditComponent,
-    RecipeStartComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-
-    ReactiveFormsModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    RecipeModule,
+    SharedModule
   ],
   providers: [RecipeService, ShoppingListService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- my-app/src/app/recipes/recipe.module.ts
+++ my-app/src/app/recipes/recipe.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
+import { RecipesComponent } from "./recipes.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { ReactiveFormsModule } from '@angular/forms';
-import {RecipesComponent} from "./recipes.component";
-import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
-import {RecipeListComponent} from "./recipe-list/recipe-list.component";
-import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
-import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
-import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
-import { DropdownDirective } from "../shared/dropdown.directive";
-import { CommonModule } from '@angular/common';
 
+import { CommonModule } from '@angular/common';
+import { RecipesRoutingModule } from './recipes-routing-module';
+import { SharedModule } from "../shared/shared.module";
 @NgModule({
   declarations: [
     RecipesComponent,
@@ -16,11 +17,13 @@ import { CommonModule } from '@angular/common';
     RecipeListComponent,
     RecipeEditComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
   ],
   imports:[
+    CommonModule,
     ReactiveFormsModule,
-    CommonModule
+    RecipesRoutingModule,
+    SharedModule
   ]
 })
 export class RecipeModule{
